2ec4eea4ab9cef6f41dca628aefd62a9f7f5890a
MoonstruckDev <lucii@moonstruck.pro>
2026-02-27 21:49:32 +0100

    added review command

--- bot/commands/topgg/vote.js
+++ bot/commands/topgg/vote.js
@@ -12,4 +12,17 @@ module.exports = {
       content: `Thank you for supporting me! You can vote here: ${voteUrl}`,
     });
   },
+
+  data: new SlashCommandBuilder()
+    .setName("review")
+    .setDescription(
+      "Get the link to leave a rewview for the bot on Top.gg (pretty please :) )",
+    ),
+
+  async execute(interaction) {
+    const voteUrl = ` https://top.gg/bot/${botId}}#reviews`;
+    await interaction.reply({
+      content: `Thank you for supporting me! You can vote here: ${voteUrl}`,
+    });
+  },
 };
