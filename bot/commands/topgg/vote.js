7958122ae847ca89a62bbd84e0f2abfed0003cd7
MoonstruckDev <lucii@moonstruck.pro>
2026-02-27 21:55:53 +0100

    fixed const + structure

--- bot/commands/topgg/vote.js
+++ bot/commands/topgg/vote.js
@@ -1,28 +1,31 @@
 const { SlashCommandBuilder } = require("discord.js");
 const { botId } = require("../../config.json");
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("vote")
-    .setDescription("Get the link to vote for the bot on Top.gg"),
+module.exports = [
+  {
+    data: new SlashCommandBuilder()
+      .setName("vote")
+      .setDescription("Get the link to vote for the bot on Top.gg"),
 
-  async execute(interaction) {
-    const voteUrl = `https://top.gg/bot/${botId}/vote`;
-    await interaction.reply({
-      content: `Thank you for supporting me! You can vote here: ${voteUrl}`,
-    });
+    async execute(interaction) {
+      const voteUrl = `https://top.gg/bot/${botId}/vote`;
+      await interaction.reply({
+        content: `Thank you for supporting me! You can vote here: ${voteUrl}`,
+      });
+    },
   },
+  {
+    data: new SlashCommandBuilder()
+      .setName("review")
+      .setDescription(
+        "Get the link to leave a rewview for the bot on Top.gg (pretty please :) )",
+      ),
 
-  data: new SlashCommandBuilder()
-    .setName("review")
-    .setDescription(
-      "Get the link to leave a rewview for the bot on Top.gg (pretty please :) )",
-    ),
-
-  async execute(interaction) {
-    const voteUrl = ` https://top.gg/bot/${botId}}#reviews`;
-    await interaction.reply({
-      content: `Thank you for supporting me! You can vote here: ${voteUrl}`,
-    });
+    async execute(interaction) {
+      const reviewUrl = ` https://top.gg/bot/${botId}}#reviews`;
+      await interaction.reply({
+        content: `Thank you for supporting me! You can vote here: ${reviewUrl}`,
+      });
+    },
   },
-};
+];
